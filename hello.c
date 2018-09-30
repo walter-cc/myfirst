6593b5eaef114c844695cbfdddbb217fd51d09c2
walter-cc <imwalter8@gmail.com>
2018-09-30 11:20:48 +0800

    add gcc compile method

--- hello.c
+++ hello.c
@@ -1,8 +1,14 @@
+/*
+	新增編譯步驟 :
+	1) gcc -c hello.c			// 產生目標檔(object file) hello.o
+	2) gcc -o hello hello.o		// 產生執行檔(execute file) hello
+	3) ./hello 					// 執行 execute file "hello"
+*/
 
 #include<stdio.h>
 
 void main(){
 
-printf("hello walter\n");
+	printf("hello walter\n");
 
 }
