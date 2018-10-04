6ae7d22c2234513f305a32a0579b018decc94976
walter-cc <imwalter8@gmail.com>
2018-10-04 13:47:05 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,8 @@
 /*
 	新增編譯步驟 :
-	1) gcc -c hello.c			// 產生目標檔(object file) hello.o
-	2) gcc -o hello hello.o		// 產生執行檔(execute file) hello
+	1) gcc -c hello.c			// 產生目標檔(object file) hello.o，-c是compiler的意思
+	2) gcc -o hello hello.o		// 產生執行檔(execute file) hello，-o是link obj的意思，-o 後面一定要接要製作出來的執行檔名稱。
+	   //gcc -o hello hello.c   or   gcc hello.c -o hello
 	3) ./hello 					// 執行 execute file "hello"
 */
 
